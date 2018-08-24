c49ebafdea7ef638849eefe6b7b20f0e640f110e
Tim Shaker <timshaker@gmail.com>
2018-08-24 01:22:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ function createRock(x) {
    * seems like a good pace.)
    */
   function moveRock() {
-    rock.style.top = `${top +=≈ 2}px`
+    rock.style.top = `${top += 2}px`
     // implement me!
     // (use the comments below to guide you!)
     /**
