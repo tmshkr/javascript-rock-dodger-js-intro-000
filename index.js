1919bd148e41b406c3ef32b4038aa87405d99471
Tim Shaker <timshaker@gmail.com>
2018-08-24 00:32:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -119,12 +119,12 @@ function moveDodger(e) {
    * we've declared for you above.)
    * And be sure to use the functions declared below!
    */
-   if (e.which === 37) {
+   if (e.which === LEFT_ARROW) {
       moveDodgerLeft()
-    } else if (e.which === 39) {
+    } else if (e.which === RIGHT_ARROW) {
       moveDodgerRight()
     }
-   
+
 }
 
 function moveDodgerLeft() {
