c463f6cd19841d054af8bdf104bb3bfcb2c5fb2a
Tim Shaker <timshaker@gmail.com>
2018-08-24 02:09:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -64,12 +64,6 @@ function createRock(x) {
   return rock
 }
 
-/**
- * End the game by clearing `gameInterval`,
- * removing all ROCKS from the DOM,
- * and removing the `moveDodger` event listener.
- * Finally, alert "YOU LOSE!" to the player.
- */
 function endGame() {
   clearInterval(gameInterval)
 
